a065af14950b830033a27465a0145325636f5961
Ajay <99019064+Ajay-2702@users.noreply.github.com>
2024-11-08 23:02:27 +0530

    added variables to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,15 @@
 variable "accountid"{
-Description="AWS account number"
+Description ="AWS account number"
 type = string 
+default = "null"
+}
+
+variable "serviceName"{
+Description ="Service name"
+type = string 
+}
+
+variable "region"{
+Description = "Region name"
+type= string 
 }
